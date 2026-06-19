44acc0f35a8f384104b456b9023770e8b64c46c9
Amit Soni <amit.soni5157@gmail.com>
2026-06-20 01:37:54 +0530

    Rhombus star pattern

--- c_Sharp/Pattern_1.cs
+++ c_Sharp/Pattern_1.cs
@@ -14,7 +14,7 @@
 //     Console.WriteLine("");
 // }
 
-Console.WriteLine("Hollow Square Star Pattern");
+// Console.WriteLine("Hollow Square Star Pattern");
 /*  
     ****
     *  *
@@ -22,18 +22,40 @@ Console.WriteLine("Hollow Square Star Pattern");
     ****
 */
 
-for(int i = 0; i< 4; i++)
+// for(int i = 0; i< 4; i++)
+// {
+//     for (int j = 0; j < 4; j++)
+//     {
+//         if((j == 1 || j == 2) && (i == 1 | i == 2))
+//         {
+//             Console.Write(" ");
+//         }
+//         else
+//         {
+//             Console.Write("*");                    
+//         }
+//     }
+//         Console.WriteLine("");     
+// }
+
+//
+Console.WriteLine("Rhombus Star Pattern");
+
+        //    ****
+        //     ****
+        //      ****
+        //       ****
+int size = 4;
+
+for (int i = 0; i < size; i++)//Row
 {
-    for (int j = 0; j < 4; j++)
-    {
-        if((j == 1 || j == 2) && (i == 1 | i == 2))
-        {
+        for (int j = 0 ; j < i; j++)
+        {    
             Console.Write(" ");
         }
-        else
+        for (int k = 0; k < size; k++)
         {
-            Console.Write("*");                    
-        }
-    }
-        Console.WriteLine("");     
+            Console.Write("*");                                     
+        }   
+        Console.WriteLine(""); 
 }
--- python/patter_1.py
+++ python/patter_1.py
@@ -10,21 +10,39 @@
 
 #         print()
 
-print("Hollow Square Star Pattern !");
-# /*  
-#     ****
-#     *  *
-#     *  *
-#     ****
-# */
+# print("Hollow Square Star Pattern !");
+# # /*  
+# #     ****
+# #     *  *
+# #     *  *
+# #     ****
+# # */
+# size = 4
+
+# for i in range(size):    
+#         for j in range(size):
+#             if i == 0 or i == size -1 or j == 0 or j == size -1:
+#                 print("*", end="")
+#             else:
+#                 print(" ", end="")
+#         print()
+        
+   
+
+
+#  print("Rhombus Star Pattern")
+ #         //    ****
+#         //     ****
+#         //      ****
+#         //       ****
 size = 4
 
-for i in range(size):    
-        for j in range(size):
-            if i == 0 or i == size -1 or j == 0 or j == size -1:
-                print("*", end="")
-            else:
-                print(" ", end="")
-        print()
-        
-         
+for i in range(size):
+    
+    for space in range(i):
+        print(" ", end="")
+
+    for k in range(size):
+        print("*", end="")
+
+    print("")
