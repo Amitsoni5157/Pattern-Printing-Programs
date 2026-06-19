92ab99cd167f14a6d59ab2efd7e702fb29dd818c
Amit Soni <amit.soni5157@gmail.com>
2026-06-19 22:23:13 +0530

    Hollow square star pattern

--- c_Sharp/Pattern_1.cs
+++ c_Sharp/Pattern_1.cs
@@ -1,15 +1,39 @@
-Console.WriteLine("Square Star Pattern");
-/*  ****
+// Console.WriteLine("Square Star Pattern");
+// /*  ****
+//     ****
+//     ****
+//     ****
+// */
+
+// for(int i = 0; i < 4; i++)
+// {
+//     for (int j = 0; j < 4; j++)
+//     {
+//      Console.Write("*");           
+//     }
+//     Console.WriteLine("");
+// }
+
+Console.WriteLine("Hollow Square Star Pattern");
+/*  
     ****
-    ****
+    *  *
+    *  *
     ****
 */
 
-for(int i = 0; i < 4; i++)
+for(int i = 0; i< 4; i++)
 {
     for (int j = 0; j < 4; j++)
     {
-     Console.Write("*");           
+        if((j == 1 || j == 2) && (i == 1 | i == 2))
+        {
+            Console.Write(" ");
+        }
+        else
+        {
+            Console.Write("*");                    
+        }
     }
-    Console.WriteLine("");
+        Console.WriteLine("");     
 }
--- python/patter_1.py
+++ python/patter_1.py
@@ -1,11 +1,30 @@
-print("Square Star Pattern")
-    # ****
-    # ****
-    # ****
-    # ****
-for i in range(4):
+# print("Square Star Pattern")
+#     # ****
+#     # ****
+#     # ****
+#     # ****
+# for i in range(4):
     
-        for j in range(4):
-            print("*", end="")
+#         for j in range(4):
+#             print("*", end="")
 
+#         print()
+
+print("Hollow Square Star Pattern !");
+# /*  
+#     ****
+#     *  *
+#     *  *
+#     ****
+# */
+size = 4
+
+for i in range(size):    
+        for j in range(size):
+            if i == 0 or i == size -1 or j == 0 or j == size -1:
+                print("*", end="")
+            else:
+                print(" ", end="")
         print()
+        
+         
