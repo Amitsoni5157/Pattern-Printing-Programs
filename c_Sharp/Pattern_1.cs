97d5be4587b18f5e582e39f6e3c54973add08ccf
Amit Soni <amit.soni5157@gmail.com>
2026-06-19 18:40:03 +0530

    square star pattern

---
+++ c_Sharp/Pattern_1.cs
@@ -0,0 +1,15 @@
+Console.WriteLine("Square Star Pattern");
+/*  ****
+    ****
+    ****
+    ****
+*/
+
+for(int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+     Console.Write("*");           
+    }
+    Console.WriteLine("");
+}
